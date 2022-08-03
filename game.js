b819095025eb1ce98b23f0a4869e0ecc35f210d2
Uziano <lucianoperaltadefazio@gmail.com>
2022-08-03 12:34:54 -0300

    Terminando

--- game.js
+++ game.js
@@ -11,30 +11,18 @@ function init() {
     }
 
     //1
-    drop1.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
-    
-    drop1.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    
+    drop1.addEventListener('dragenter', function(e){ e.preventDefault(); }, false);
+    drop1.addEventListener('dragover', function(e){ e.preventDefault(); }, false);
     drop1.addEventListener('drop', soltado, false);
 
     //2
-    drop2.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
-   
-    drop2.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    
+    drop2.addEventListener('dragenter', function(e){ e.preventDefault(); }, false);
+    drop2.addEventListener('dragover', function(e){ e.preventDefault(); }, false);
     drop2.addEventListener('drop', soltado2, false);
 
     //3
-    drop3.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
-    
-    drop3.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    
+    drop3.addEventListener('dragenter', function(e){ e.preventDefault(); }, false);
+    drop3.addEventListener('dragover', function(e){ e.preventDefault(); }, false);
     drop3.addEventListener('drop', soltado3, false);
 
 }
@@ -43,9 +31,10 @@ function arrastrado(e){
     element = e.target;
     e.dataTransfer.setData('Text', element.getAttribute('id'));
 }
+
 function finalizado(e){
     element = e.target;
-    element.style.visibility='hidden';
+    element.style.visibility = 'hidden';
 } 
 
 function soltado(e){
@@ -53,9 +42,9 @@ function soltado(e){
     var id = e.dataTransfer.getData('Text');
     if (id!="imagen4"){
         var src = document.getElementById(id).src;
-    drop1.innerHTML='<img src="'+src+'" height="400px" width="275px">';
+        drop1.innerHTML='<img src="'+src+'" height="400px" width="275px">';
     } else {
-    drop1.innerHTML='la imagen no es admitida';
+        drop1.innerHTML='la imagen no es admitida';
     }
 }
 
@@ -64,9 +53,9 @@ function soltado2(e){
     var id=e.dataTransfer.getData('Text');
     if(id!="imagen4"){
         var src=document.getElementById(id).src;
-    drop2.innerHTML='<img src="'+src+'"height="400px" width="275px">';
+        drop2.innerHTML='<img src="'+src+'"height="400px" width="275px">';
     }else{
-    drop2.innerHTML='la imagen no es admitida';
+        drop2.innerHTML='la imagen no es admitida';
     }
 }
 function soltado3(e){
@@ -74,9 +63,9 @@ function soltado3(e){
     var id=e.dataTransfer.getData('Text');
     if(id!="imagen4"){
         var src=document.getElementById(id).src;
-    drop3.innerHTML='<img src="'+src+'"height="400px" width="275px">';
+        drop3.innerHTML='<img src="'+src+'"height="400px" width="275px">';
     }else{
-    drop3.innerHTML='la imagen no es admitida';
+        drop3.innerHTML='la imagen no es admitida';
     }
 }
 
